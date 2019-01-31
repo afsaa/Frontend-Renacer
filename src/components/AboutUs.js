74d6d85970058071c52b18fd27d1081d57841b98
Andres Saa <andressaa.dev@gmail.com>
2019-01-31 18:31:03 -0500

    Página Quienes Somos - Modificaciones

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -11,19 +11,30 @@ Container.propTypes = {
 
 export default function AboutUs() {
 const BgOverlay = {
-    background: "linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url("+ AboutBg +")",
+    position:"relative",
+    background:"linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url("+ AboutBg +")",
     backgroundRepeat:"no-repeat",
     backgroundSize:"cover",
     backgroundPosition:"center center",
     color:"#fff",
     height:"100vh",
-    paddingTop:"40vh"
+    paddingTop:"20vh"
 };
   return (
     <React.Fragment>
         <Container fluid style={BgOverlay} className="text-center">
-            <h1>Quienes Somos</h1>
-            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur doloribus, assumenda dignissimos deleniti in!</h4>
+            <section className="my-3">
+                <h3>Quienes Somos</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos tenetur doloribus, assumenda dignissimos deleniti in!</p>
+            </section>
+            <section className="my-3">
+                <h3>Misión</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit accusantium sunt quam aliquam praesentium ratione porro, corporis est dolorum id?</p>
+            </section>
+            <section className="my-3">
+                <h3>Visión</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo distinctio debitis facilis, deserunt unde dolore quidem autem eligendi fuga corrupti.</p>
+            </section>
         </Container>
     </React.Fragment>
   )
