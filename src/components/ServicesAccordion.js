3bc03e73a4ff57a53696fe3152daefd8a30226a5
Andres Saa <andressaa.dev@gmail.com>
2019-02-19 10:48:18 -0500

    Página de servicios - Cubierta de imagen con frase relajante agregada

--- src/components/ServicesAccordion.js
+++ src/components/ServicesAccordion.js
@@ -1,20 +1,38 @@
 import React, { Component, Fragment } from 'react';
-import { Button } from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import ServiceItem from './ServiceItem';
 import PropTypes from "prop-types";
+import ServicesBg from '../img/services/servicesOverlay.jpeg';
 
 library.add(faArrowLeft);
+
+Container.propTypes = {
+  fluid: PropTypes.bool
+  // applies .container-fluid class
+};
+
+const BgOverlay = {
+  position: "absolute",
+  background: "linear-gradient(rgba(0,0,0,.7), rgba(0,0,0,.7)), url(" + ServicesBg + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center center",
+  color: "#fff",
+  width: "100%",
+  height: "56%",
+  paddingTop: "10%"
+};
 
 export class ServicesAccordion extends Component {
   render() {
     return (
       <Fragment>
         <Link to="/"><Button color="link" className="mt-0"><FontAwesomeIcon icon="arrow-left" /> Volver</Button></Link>
-        <ul style={{ padding: "0" }}>
+        <ul style={{ padding:"0", marginBottom:"0" }}>
             {this.props.services.map(service => {
                 return (
                     <ServiceItem
@@ -28,6 +46,12 @@ export class ServicesAccordion extends Component {
                 );
             })}
         </ul>
+        <Container fluid style={ BgOverlay } className="text-center">
+          <blockquote className="blockquote text-center">
+            <p className="mb-0">Allí donde el agua alcanza su mayor profundidad, se mantiene más en calma.</p>
+            <footer className="blockquote-footer">William Shakespeare </footer>
+          </blockquote>
+        </Container>
         </Fragment>
     )
   }
