fb35012e91bacf271c4611f2bac051fd88141fca
Andres Saa <andressaa.dev@gmail.com>
2020-04-08 17:34:14 -0500

    Organizando imágenes de la página de servicios

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 import Home from "./components/pages/Home";
 import AboutUs from "./components/pages/AboutUs";
 import ServicesAccordion from "./components/ServicesAccordion";
-import Contact from './components/pages/Contact';
-import FormSuccess from './components/pages/FormSuccess';
+import Contact from "./components/pages/Contact";
+import FormSuccess from "./components/pages/FormSuccess";
 import { Switch, Route } from "react-router-dom";
-import uuid from 'uuid/v1';
+import uuid from "uuid/v1";
 // Reacstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 // Font Awesome Icons
@@ -23,24 +23,96 @@ class App extends Component {
     this.state = {
       services: [
         {
-          id: uuid(), name: "Organizacional", subCategories: ["Gestión del Talento Humano", "Capacitación y Aprendizaje Organizacional", "Psicología de la Salud Ocupacional y Factores de Riesgo Psicosocial"], specificServices: [["Manejo del estrés laboral", "Gestión por competencias", "Técnicas de relajación"], ["Análisis y descripción de cargos", "Comunicación Efectiva", "Intervención Individuo, Familia y Organización"], ["Selección de personal", "Atención al cliente", "Manejo de adicciones"]], cardImgSrc: "https://via.placeholder.com/300x180", collapse: false},
-        { id: uuid(), name: "Educativo", subCategories: [""], specificServices: [["Escuelas de padres"], ["Elaboración de proyectos de vida"], ["Orientación vocacional"]], cardImgSrc: "https://via.placeholder.com/500", collapse: false},
-        { id: uuid(), name: "Social", subCategories: ["Procesos de Desarrollo Comunitario"], specificServices: [["Charlas y conferencias dedicadas a la comunidad"], ["Diseño e implementación de proyectos"], ["Participación en licitaciones"]], cardImgSrc: "https://via.placeholder.com/500",collapse: false},
-        { id: uuid(), name: "Clínico", subCategories: [""], specificServices: [["Psicoterapia a personas con trastornos psicológicos"], ["Asesoría psicológica individual"], ["Seminarios, convivencia y talleres vivenciales"]], cardImgSrc: "https://via.placeholder.com/500",collapse: false},
-        { id: uuid(), name: "Otros", subCategories: [""], specificServices: [["Clases de yoga"], ["Arte terapia"], ["Reflexología"]], cardImgSrc: "https://via.placeholder.com/500",collapse: false}
-      ]
-    }
+          id: uuid(),
+          name: "Organizacional",
+          subCategories: [
+            "Gestión del Talento Humano",
+            "Capacitación y Aprendizaje Organizacional",
+            "Psicología de la Salud Ocupacional y Factores de Riesgo Psicosocial",
+          ],
+          specificServices: [
+            [
+              "Manejo del estrés laboral",
+              "Gestión por competencias",
+              "Técnicas de relajación",
+            ],
+            [
+              "Análisis y descripción de cargos",
+              "Comunicación Efectiva",
+              "Intervención Individuo, Familia y Organización",
+            ],
+            [
+              "Selección de personal",
+              "Atención al cliente",
+              "Manejo de adicciones",
+            ],
+          ],
+          cardImgSrc: "https://via.placeholder.com/300x180",
+          collapse: false,
+        },
+        {
+          id: uuid(),
+          name: "Educativo",
+          subCategories: [""],
+          specificServices: [
+            ["Escuelas de padres"],
+            ["Elaboración de proyectos de vida"],
+            ["Orientación vocacional"],
+          ],
+          cardImgSrc: "https://via.placeholder.com/500",
+          collapse: false,
+        },
+        {
+          id: uuid(),
+          name: "Social",
+          subCategories: ["Procesos de Desarrollo Comunitario"],
+          specificServices: [
+            ["Charlas y conferencias dedicadas a la comunidad"],
+            ["Diseño e implementación de proyectos"],
+            ["Participación en licitaciones"],
+          ],
+          cardImgSrc: "https://via.placeholder.com/500",
+          collapse: false,
+        },
+        {
+          id: uuid(),
+          name: "Clínico",
+          subCategories: [""],
+          specificServices: [
+            ["Psicoterapia a personas con trastornos psicológicos"],
+            ["Asesoría psicológica individual"],
+            ["Seminarios, convivencia y talleres vivenciales"],
+          ],
+          cardImgSrc: "https://via.placeholder.com/500",
+          collapse: false,
+        },
+        {
+          id: uuid(),
+          name: "Otros",
+          subCategories: [""],
+          specificServices: [
+            ["Clases de yoga"],
+            ["Arte terapia"],
+            ["Reflexología"],
+          ],
+          cardImgSrc: "https://via.placeholder.com/500",
+          collapse: false,
+        },
+      ],
+    };
     this.showInfo = this.showInfo.bind(this);
   }
 
   showInfo = (id) => {
-    this.setState({ services: this.state.services.map( service => { 
-      if (service.id === id) {
-        service.collapse = !service.collapse
-      }
-      return service;
-      }) });
-  }
+    this.setState({
+      services: this.state.services.map((service) => {
+        if (service.id === id) {
+          service.collapse = !service.collapse;
+        }
+        return service;
+      }),
+    });
+  };
 
   render() {
     return (
@@ -48,7 +120,15 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/quienes-somos" component={AboutUs} />
-          <Route path="/servicios" render={props => <ServicesAccordion services={this.state.services} showInfo={this.showInfo} />} />
+          <Route
+            path="/servicios"
+            render={(props) => (
+              <ServicesAccordion
+                services={this.state.services}
+                showInfo={this.showInfo}
+              />
+            )}
+          />
           <Route exact path="/contacto" component={Contact} />
           <Route path="/exito" component={FormSuccess} />
         </Switch>
--- src/components/ServiceItem.js
+++ src/components/ServiceItem.js
@@ -1,43 +1,68 @@
-import React, { Component } from 'react'
-import { Container, Button, Collapse, Card, CardDeck, CardImg, CardBody, CardTitle } from 'reactstrap';
+import React, { Component } from "react";
+import {
+  Container,
+  Button,
+  Collapse,
+  Card,
+  CardDeck,
+  CardImg,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 import PropTypes from "prop-types";
+
+import orgImage from "../img/services/hr-cat-image.jpg";
+import eduImage from "../img/services/education-cat-image.jpg";
+import healthImage from "../img/services/health-cat-image.jpg";
 
 export class ServiceItem extends Component {
   render() {
     const id = this.props.id;
+    const imagesArray = [orgImage, eduImage, healthImage];
+    const catImagesArray = [healthImage];
+
     return (
-        <li style={{listStyleType:"none"}}>
-            <Button color="primary" className="btn-lg" block onClick={ this.props.showInfo.bind(this,id) }>{this.props.name}</Button>
-            <Collapse isOpen={this.props.collapse}>
-              <Container className="my-3">
-                <CardDeck>
-                  {
-                    this.props.subCategories.map( (cardTitle, index) => 
-                      <Card>
-                        <CardImg top width="100%" src={this.props.cardImgSrc} alt="Card image cap" />
-                        <CardBody>
-                          <CardTitle>{ cardTitle }</CardTitle>
-                          {
-                            this.props.specificServices.map( cardItem => 
-                              <ul className="list-group list-group-flush">
-                                <li className="list-group-item">{ cardItem[index] }</li>
-                              </ul>
-                              )
-                          }
-                        </CardBody>
-                      </Card>
-                    )
-                  }
-                </CardDeck>
-              </Container>
-            </Collapse>
-        </li>
-    )
+      <li style={{ listStyleType: "none" }}>
+        <Button
+          color="primary"
+          className="btn-lg"
+          block
+          onClick={this.props.showInfo.bind(this, id)}
+        >
+          {this.props.name}
+        </Button>
+        <Collapse isOpen={this.props.collapse}>
+          <Container className="my-3">
+            <CardDeck>
+              {this.props.subCategories.map((cardTitle, index) => (
+                <Card key={index}>
+                  {index < 4 ? (
+                    <CardImg top width="100%" src={imagesArray[index]} />
+                  ) : (
+                    <CardImg top width="100%" src={catImagesArray[index]} />
+                  )}
+                  <CardBody key={index}>
+                    <CardTitle key={index}>{cardTitle}</CardTitle>
+                    {this.props.specificServices.map((cardItem, indexc) => (
+                      <ul key={indexc} className="list-group list-group-flush">
+                        <li key={indexc} className="list-group-item">
+                          {cardItem[index]}
+                        </li>
+                      </ul>
+                    ))}
+                  </CardBody>
+                </Card>
+              ))}
+            </CardDeck>
+          </Container>
+        </Collapse>
+      </li>
+    );
   }
 }
 
 ServiceItem.propTypes = {
-  showInfo: PropTypes.func.isRequired
+  showInfo: PropTypes.func.isRequired,
 };
 
 CardImg.propTypes = {
@@ -46,7 +71,7 @@ CardImg.propTypes = {
   className: PropTypes.string,
   // Use top or bottom to position image via "card-img-top" or "card-img-bottom"
   top: PropTypes.bool,
-  bottom: PropTypes.bool
+  bottom: PropTypes.bool,
 };
 
 export default ServiceItem;
